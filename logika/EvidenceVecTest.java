d8244bd6efd873121e829a7f1af58c3280321561
adam <adam.stupka@gmail.com>
2022-05-10 15:01:48 +0200

    test test
    Win condition
    
    SPawning items
    
    Prolog
    
    Prvni cast poznamek
    
    Komplet poznamky
    
    Test Evidence
    
    Metoda Dohrano
    
    Readme.txt
    
    + Test + Testy Hry
    
    + Popisy predmetu
    
    + Skřítek + prohra
    
    + Truhla
    
    +Jar file
    
    + First revision

--- logika/EvidenceVecTest.java
+++ logika/EvidenceVecTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Třída sloužící k otestování evidence věcí a batohu
+ * @author Adam Stupka
+ */
+
 public class EvidenceVecTest {
 
     private Hra hra;
@@ -16,21 +21,30 @@ public class EvidenceVecTest {
     @Before
     public void setUp() {
         hra = new Hra();
-        prostor = new Prostor("Hala","TEST");
         vec1 = new Vec("MEC", prostor, true,"Ostrý meč");
         vec2 = new Vec("STŮL", prostor, false, "Težky stůl");
         evidenceVeci = new EvidenceVeci();
     }
 
+    //Test batohu a předmětů
+
     @Test
-    public void testManipulace(){
-        assertEquals(true, evidenceVeci.vlozVec(vec1));
-        assertEquals(true, evidenceVeci.vlozVec(vec2));
-        //assertEquals("[" + vec1.getNazev() + ", " + vec2.getNazev() + "]", evidenceVeci.vratSeznamVeciVMistnosti(prostor).toString());
-        assertEquals(null, evidenceVeci.predmetVRuce());
-        assertEquals(vec1, evidenceVeci.getUrcityPredmet("MEC"));
-        evidenceVeci.getUrcityPredmet("MEC").setProstor(null);
-        assertEquals(vec1, evidenceVeci.predmetVRuce());
+    public void testPrubehHry() {
+        assertEquals("Hala", hra.getHerniPlan().getAktualniProstor().getNazev());
+
+        hra.zpracujPrikaz("jdi Obývací_pokoj");
+        assertEquals(false, hra.konecHry());
+        assertEquals("Obývací_pokoj", hra.getHerniPlan().getAktualniProstor().getNazev());
+        assertEquals("Držim MEDVÍDEK", hra.zpracujPrikaz("interakce MEDVÍDEK"));
+        assertEquals("V ruce držím MEDVÍDEK\nChlupatý plyšový medvídek", hra.zpracujPrikaz("inventar"));
+        assertEquals("Neunesu už další věc", hra.zpracujPrikaz("interakce SKLENICE"));
+        assertEquals("Pustil jsem MEDVÍDEK", hra.zpracujPrikaz("pust"));
+        assertEquals("V ruce nic nedržím", hra.zpracujPrikaz("inventar"));
+
+        //Nelze sebrat velké předměty
+        assertEquals("Jdou slyšet krásné noty, čím víc se přibližuji tím méně hraje. Když se k němu postavím... ztichlo", hra.zpracujPrikaz("interakce PIANO"));
+
     }
+
 
 }
